1b7bf6eb59ace54f4d67fb0883707f16d5ce157a
Showmil <hilyhily334@gmail.com>
2025-03-18 00:53:08 +0900

    test: yaml 파일 수정 #2

--- src/db.ts
+++ src/db.ts
@@ -9,6 +9,7 @@ const connection = mysql.createConnection({
   port: Number(process.env.DB_PORT),
   database: 'pyeonhaeng',
   dateStrings: true,
+  enableKeepAlive: true
 });
 
 module.exports = connection;
